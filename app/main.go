1578135acfbdb36bf07f8c32f12108a235df3490
Vikranth Kumar <vikranthkumar0123@gmail.com>
2025-03-30 22:09:18 +0530

    add support for other character tokenization

--- app/main.go
+++ app/main.go
@@ -5,6 +5,49 @@ import (
 	"os"
 )
 
+const (
+	LEFT_PAREN  rune = '('
+	RIGHT_PAREN rune = ')'
+	LEFT_BRACE  rune = '{'
+	RIGHT_BRACE rune = '}'
+	STAR        rune = '*'
+	COMMA       rune = ','
+	DOT         rune = '.'
+	MINUS       rune = '-'
+	PLUS        rune = '+'
+	SEMICOLON   rune = ';'
+)
+
+func tokenizeString(inp string) (tokens []string) {
+	tokens = make([]string, 0)
+	for _, char := range inp {
+		switch char {
+		case LEFT_PAREN:
+			tokens = append(tokens, "LEFT_PAREN ( null")
+		case RIGHT_PAREN:
+			tokens = append(tokens, "RIGHT_PAREN ) null")
+		case LEFT_BRACE:
+			tokens = append(tokens, "LEFT_BRACE { null")
+		case RIGHT_BRACE:
+			tokens = append(tokens, "RIGHT_BRACE } null")
+		case STAR:
+			tokens = append(tokens, "STAR * null")
+		case COMMA:
+			tokens = append(tokens, "COMMA , null")
+		case DOT:
+			tokens = append(tokens, "DOT . null")
+		case MINUS:
+			tokens = append(tokens, "MINUS - null")
+		case PLUS:
+			tokens = append(tokens, "PLUS + null")
+		case SEMICOLON:
+			tokens = append(tokens, "SEMICOLON ; null")
+		}
+	}
+	tokens = append(tokens, "EOF  null")
+	return
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
@@ -30,22 +73,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	tokens := []string{}
 	if len(fileContents) > 0 {
-		for _, char := range string(fileContents) {
-			switch char {
-			case '(':
-				tokens = append(tokens, "LEFT_PAREN ( null")
-			case ')':
-				tokens = append(tokens, "RIGHT_PAREN ) null")
-			case '{':
-				tokens = append(tokens, "LEFT_BRACE { null")
-			case '}':
-				tokens = append(tokens, "RIGHT_BRACE } null")
-			}
-		}
-		tokens = append(tokens, "EOF  null")
-
+		tokens := tokenizeString(string(fileContents))
 		for _, token := range tokens {
 			fmt.Fprintln(os.Stdout, token)
 		}
